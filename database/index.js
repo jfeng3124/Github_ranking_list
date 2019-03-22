2cc76924183288961fdb53950e5f9c90193862fc
Jia <jiafeng3124@gmail.com>
2019-03-22 12:30:34 -0700

    implement schema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  repos: {
+    owner: String,
+    html_url: String,
+    updated_at: Date
+  }
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
